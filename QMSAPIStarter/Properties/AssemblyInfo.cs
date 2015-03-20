c89c43fc5abb58430c8c6782b08d71fe83e2a9e1
countnazgul <stefan.stoichev@gmail.com>
2015-03-20 16:47:50 +0000

    initial commit

---
+++ QMSAPIStarter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QV_GetActiveUsersDocs")]
+[assembly: AssemblyDescription("Export the active QVS Users<->Docs in csv")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("stefan.stoichev@gmail.com")]
+[assembly: AssemblyProduct("QV_GetActiveUsersDocs")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a91a126e-c856-475a-9c5d-404ec3ebc68b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
